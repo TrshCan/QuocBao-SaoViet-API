e886a7e709b83e29aea91029e455271a49753a23
quan3473 <nguyenminhquan042004@gmail.com>
2025-11-06 22:30:08 +0700

    Feat: Add PrismaModule and enhance PrismaService with lifecycle hooks and logging

--- src/modules/shared/prisma/prisma.service.ts
+++ src/modules/shared/prisma/prisma.service.ts
@@ -1,10 +1,58 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { envConfig } from '@/configs/config-env';
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy, OnApplicationBootstrap
+{
+  constructor() {
+    super({
+      log: [
+        {
+          level: 'warn',
+          emit: 'event',
+        },
+        {
+          level: 'info',
+          emit: 'event',
+        },
+        {
+          level: 'error',
+          emit: 'event',
+        },
+      ],
+      errorFormat: envConfig.NODE_ENV === 'production' ? 'minimal' : 'pretty',
+    });
+  }
+
   async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  onApplicationBootstrap() {
+    this.$connect()
+      .then(() => {
+        console.log(
+          `[${envConfig.NODE_ENV}] - PrismaService - Connected to database`,
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `[${envConfig.NODE_ENV}] - PrismaService - Error connecting to database`,
+          error,
+        );
+      });
   }
 }
