81ebc0d818ed8fbc7bafd284b10f9349f5848f11
quan3473 <nguyenminhquan042004@gmail.com>
2025-11-11 02:44:37 +0700

    Feat: Add user retrieval and update methods in UserRepository; implement error handling and logging in UserService for improved user management and service reliability.

--- src/modules/identity/user/user.repository.ts
+++ src/modules/identity/user/user.repository.ts
@@ -25,4 +25,57 @@ export class UserRepository {
     })) as Prisma.UserGetPayload<{ select: T; include: I }> | null;
     return buildQuery;
   }
+
+  async findOneById<
+    T extends Prisma.UserSelect<DefaultArgs> | undefined,
+    I extends Prisma.UserInclude<DefaultArgs> | undefined,
+  >(
+    id: string,
+    options?: {
+      select?: T;
+      include?: I;
+    },
+  ): Promise<Prisma.UserGetPayload<{ select: T; include: I }> | null> {
+    const buildQuery = (await this.prisma.user.findUnique({
+      where: { id },
+      ...options,
+    })) as Prisma.UserGetPayload<{ select: T; include: I }> | null;
+    return buildQuery;
+  }
+
+  async findOneByEmail<
+    T extends Prisma.UserSelect<DefaultArgs> | undefined,
+    I extends Prisma.UserInclude<DefaultArgs> | undefined,
+  >(
+    email: string,
+    options?: {
+      select?: T;
+      include?: I;
+    },
+  ): Promise<Prisma.UserGetPayload<{ select: T; include: I }> | null> {
+    const buildQuery = (await this.prisma.user.findUnique({
+      where: { email },
+      ...options,
+    })) as Prisma.UserGetPayload<{ select: T; include: I }> | null;
+    return buildQuery;
+  }
+
+  async updateOneById<
+    T extends Prisma.UserSelect<DefaultArgs> | undefined,
+    I extends Prisma.UserInclude<DefaultArgs> | undefined,
+  >(
+    id: string,
+    data: Prisma.UserUpdateInput,
+    options?: {
+      select?: T;
+      include?: I;
+    },
+  ): Promise<Prisma.UserGetPayload<{ select: T; include: I }> | null> {
+    const buildQuery = (await this.prisma.user.update({
+      where: { id },
+      data,
+      ...options,
+    })) as Prisma.UserGetPayload<{ select: T; include: I }> | null;
+    return buildQuery;
+  }
 }
--- src/modules/identity/user/user.service.ts
+++ src/modules/identity/user/user.service.ts
@@ -1,7 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { UserRepository } from './user.repository';
+import { toErrorMessage } from '@/utils';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(private userRepository: UserRepository) {}
+
+  async getCurrentUser(userId: string) {
+    try {
+      const user: FoundCurrentUser | null =
+        await this.userRepository.findOneById(userId, {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+            fullName: true,
+            phone: true,
+            role: true,
+            permissions: true,
+            status: true,
+          },
+        });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      this.logger.error('Failed to get current user:', toErrorMessage(error));
+      throw new InternalServerErrorException('Failed to get current user');
+    }
+  }
 }
+
+export type FoundCurrentUser = Pick<
+  Prisma.UserGetPayload<{
+    select: {
+      id: true;
+      username: true;
+      email: true;
+      fullName: true;
+      phone: true;
+      role: true;
+      permissions: true;
+      status: true;
+    };
+  }>,
+  | 'id'
+  | 'username'
+  | 'email'
+  | 'fullName'
+  | 'phone'
+  | 'role'
+  | 'permissions'
+  | 'status'
+>;
