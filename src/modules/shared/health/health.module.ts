5e37fc130667dc3d1059df95c97913f8d99eeaa8
quan3473 <nguyenminhquan042004@gmail.com>
2025-11-17 14:44:35 +0700

    Refactor: Enhance HealthController to integrate CacheManagerHealthIndicator and add Redis health check endpoint. Update HealthModule imports to include CacheManagerModule.

--- src/modules/shared/health/health.module.ts
+++ src/modules/shared/health/health.module.ts
@@ -3,11 +3,20 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 
 import { HealthController } from './health.controller';
-import { IoredisModule } from '../ioredis/ioredis.module';
+// import { IoredisModule } from '../ioredis/ioredis.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CacheManagerModule } from '../cache-manager';
+import { IoredisModule } from '../ioredis';
 
 @Module({
-  imports: [TerminusModule, HttpModule, IoredisModule, PrismaModule],
+  imports: [
+    TerminusModule,
+    HttpModule,
+    PrismaModule,
+    CacheManagerModule,
+    HealthModule,
+    IoredisModule,
+  ],
   controllers: [HealthController],
 })
 export class HealthModule {}
