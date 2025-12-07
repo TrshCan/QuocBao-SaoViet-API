0abe026d09eda526314ee207cc4e5133d6532060
quan3473 <nguyenminhquan042004@gmail.com>
2025-12-07 13:06:37 +0700

    feat: Implement response transformation schema and DTO for enhanced API response validation and Swagger documentation

--- src/common/validations/response-transform.ts
+++ src/common/validations/response-transform.ts
@@ -1,7 +1,38 @@
-export type ResponseTransform<T> = {
-  message?: string;
-  data?: T;
-  statusCode: number;
-  success: boolean;
-  timestamp: string;
-};
+import { ApiProperty } from '@nestjs/swagger';
+import { z } from 'zod/v4';
+
+export const responseTransformSchema = (dataSchema: z.ZodType) =>
+  z.object({
+    message: z.string().optional(),
+    data: dataSchema,
+    statusCode: z.number(),
+    success: z.boolean(),
+    timestamp: z.string(),
+  });
+
+export function ResponseTransformDto<
+  TModel extends new (...args: any[]) => any,
+>(model: TModel) {
+  class ClassResponseTransformDto {
+    @ApiProperty()
+    message: string;
+
+    @ApiProperty({ type: model, required: false })
+    data: InstanceType<TModel>;
+
+    @ApiProperty()
+    statusCode: number;
+
+    @ApiProperty()
+    success: boolean;
+
+    @ApiProperty()
+    timestamp: string;
+  }
+
+  Object.defineProperty(ClassResponseTransformDto, 'name', {
+    value: `${model.name}Response`,
+  });
+
+  return ClassResponseTransformDto;
+}
